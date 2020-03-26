8048273c5e2987cb29fab44fa23095351d94de19
giova <fabiani.giovanni@gmail.com>
2020-03-26 15:20:32 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        var main =new Main();
+        for(int j=0;j<10;j++)
+            System.out.println(main.count(j));
+    }
+    int count(int i){
+        return i;
+    }
+}
